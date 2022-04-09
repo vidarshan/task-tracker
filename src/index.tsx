995ee2f014ba619f6ec03c29e8317e4269e7bc34
vidarshan <vidarshandithya3@gmail.com>
2022-04-09 18:27:30 +0530

    Added appshel

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

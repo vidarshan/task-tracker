f086678cc5e9727950e0c5a3f34c7572593051fa
vidarshanadithya <vidarshanadithya3@gmail.com>
2022-04-10 21:12:43 +0530

    Changes to task tabs

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -11,37 +11,45 @@ import {
   Tabs,
   Textarea,
   TextInput,
+  Title,
 } from "@mantine/core";
 import { DatePicker, TimeInput } from "@mantine/dates";
 import React, { useState } from "react";
 import {
   BiCalendar,
+  BiCheck,
   BiListCheck,
   BiListMinus,
+  BiMinus,
   BiParagraph,
   BiStar,
   BiText,
+  BiTime,
   BiTimer,
 } from "react-icons/bi";
 import Task from "../components/Task";
 import Layout from "../layout/Layout";
 
 const Tasks = () => {
-  const [opened, setOpened] = useState(false);
-
   return (
     <Layout>
-      <Button onClick={() => setOpened(true)}>Open</Button>
-      <Tabs color="violet" variant="default" tabPadding="xl">
-        <Tabs.Tab label="Current">
+      <Title mb={20} order={3}>
+        Tasks
+      </Title>
+      <Tabs color="grape" variant="default" tabPadding="xl">
+        <Tabs.Tab icon={<BiTime />} label="Current">
           <Grid>
             <Col span={12}>
               <Task />
             </Col>
           </Grid>
         </Tabs.Tab>
-        <Tabs.Tab label="Completed">Messages tab content</Tabs.Tab>
-        <Tabs.Tab label="Hold">Settings tab content</Tabs.Tab>
+        <Tabs.Tab icon={<BiCheck />} label="Completed">
+          Messages tab content
+        </Tabs.Tab>
+        <Tabs.Tab icon={<BiMinus />} label="Hold">
+          Settings tab content
+        </Tabs.Tab>
       </Tabs>
     </Layout>
   );

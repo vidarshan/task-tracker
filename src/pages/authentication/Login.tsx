deaa3d9f0243b6ae3eca87b3c43a36ca413d70ed
vidarshanadithya <vidarshanadithya3@gmail.com>
2022-04-10 19:25:52 +0530

    Added login page and controls

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,7 +1,57 @@
+import {
+  Box,
+  Button,
+  Card,
+  Col,
+  Container,
+  Grid,
+  Group,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
 import React from "react";
+import { BiAt, BiKey } from "react-icons/bi";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "95vh",
+      }}
+    >
+      <Card sx={{ minWidth: "400px" }} shadow="xl" radius="md" withBorder>
+        <Title order={4}>Log In</Title>
+        <Text size="sm">Welcome Back</Text>
+        <TextInput
+          icon={<BiAt />}
+          mt={20}
+          radius="md"
+          label="Your Email"
+          placeholder="Email"
+          required
+        />
+        <PasswordInput
+          icon={<BiKey />}
+          mt={20}
+          radius="md"
+          label="Your Password"
+          placeholder="Password"
+          required
+        />
+        <Button mt={20} color="grape" radius="md" fullWidth>
+          Log In
+        </Button>
+        <Button mt={20} variant="subtle" radius="md" fullWidth>
+          New User?
+        </Button>
+      </Card>
+    </Box>
+  );
 };
 
 export default Login;

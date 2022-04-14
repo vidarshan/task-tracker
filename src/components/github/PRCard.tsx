916450207e5a7784890594547f7b92e64cf054d8
vidarshanadithya <vidarshanadithya3@gmail.com>
2022-04-14 18:33:12 +0530

    Added PR card on github

---
+++ src/components/github/PRCard.tsx
@@ -0,0 +1,32 @@
+import { Button, Card, Group, Text, ThemeIcon } from "@mantine/core";
+import React from "react";
+import { BiLink } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
+
+const PRCard = () => {
+  return (
+    <Card mt={20} radius="md" shadow="xl" withBorder>
+      <Group direction="row" position="apart">
+        <Group>
+          <ThemeIcon variant="light" radius="xl" size="lg" color="green">
+            <FaGithub />
+          </ThemeIcon>
+          <span>
+            <Text size="xs" weight={600}>
+              vidarshanadithya/task-tracker
+            </Text>
+            <Text color="cyan" mt={10} weight={700}>
+              Implemented Sign Up
+            </Text>
+          </span>
+        </Group>
+
+        <Button color="grape" leftIcon={<BiLink />} radius="md">
+          View
+        </Button>
+      </Group>
+    </Card>
+  );
+};
+
+export default PRCard;
--- src/pages/github/Github.tsx
+++ src/pages/github/Github.tsx
@@ -1,7 +1,19 @@
+import { Col, Grid, Title } from "@mantine/core";
 import React from "react";
+import PRCard from "../../components/github/PRCard";
+import Layout from "../../layout/Layout";
 
 const Github = () => {
-  return <div>Github</div>;
+  return (
+    <Layout>
+      <Title order={4}>Pull Requests</Title>
+      <Grid>
+        <Col span={12}>
+          <PRCard />
+        </Col>
+      </Grid>
+    </Layout>
+  );
 };
 
 export default Github;
